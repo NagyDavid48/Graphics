0ae3dedc7c95533366592f6649e7e1c6dfc9ca86
pgpgpg777 <pappbogar@gmail.com>
2015-05-10 23:58:34 +0200

    GValamik

--- GKisRobot.java
+++ GKisRobot.java
@@ -1,6 +1,10 @@
 package Graphics;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class GKisRobot implements RajzolAble {
 
@@ -8,6 +12,11 @@ public class GKisRobot implements RajzolAble {
 	protected int z;
 	protected KisRobot kisRobot;
 
+	
+	GKisRobot() throws IOException{
+		img = ImageIO.read(new File("Kepek/Cleaner_robot.jpg"));
+	}
+	
 	public void rajzol() {
 		// TODO - implement GKisRobot.rajzol
 		throw new UnsupportedOperationException();
