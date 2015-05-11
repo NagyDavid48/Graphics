5d1521985d252155281d1be376b027aecc43ea3a
CsTomi <cstom@sch.bme.hu>
2015-05-11 11:21:56 +0200

    Mozgás update
    Mozgás javítva.

--- GKisRobot.java
+++ GKisRobot.java
@@ -18,39 +18,22 @@ public class GKisRobot implements RajzolAble {
 	GKisRobot() throws IOException{
 		img = ImageIO.read(new File("Kepek/Cleaner_robot.jpg"));
 		elozo = new Vektor(kisRobot.getMezo().getPoziciovektor().getX(), 
-						   kisRobot.getMezo().getPoziciovektor().getY());//itt mÈg a jelenlegi poziciÛval egyenlı
+						   kisRobot.getMezo().getPoziciovektor().getY());//itt m√©g a jelenlegi pozici√≥val egyenl≈ë
 	}
 	
-	/**
-	 * Mozgat·st az egyenes ir·ny vektoros egyenletÈvel oldom meg.
-	 * Uj(x1, y1)
-	 * Elozo(x0, y0)
-	 * Irany(x1-x0, y1-y0)
-	 * Amelyik pont az egyenesre illeszkedik, arra megy.
-	 */
 	public void rajzol(Graphics g) {
-		
 		Vektor uj = new Vektor(kisRobot.getMezo().getPoziciovektor().getX(),
-							   kisRobot.getMezo().getPoziciovektor().getY());//Megv·ltozott poziciÛ
-		if(uj.getX()!=elozo.getX() || uj.getY()!=elozo.getY()){//ha v·ltoz·s van, akkor kirajzoljuk a mozg·st
-			Vektor iranyVektor = new Vektor(uj.getX()-elozo.getX(), uj.getY()-elozo.getY());
-			
-			Vektor egyenletBal = new Vektor(iranyVektor.getY(), iranyVektor.getX());//v2*x-v1*y
-			int egyenletJobb = iranyVektor.getY()*elozo.getX()-iranyVektor.getY()*elozo.getY();//a konstans
-			
-			int i = 1;
-			while(elozo.getX()==uj.getX() && elozo.getY()==uj.getY()){
-				if(egyenletJobb == (egyenletBal.getY()*i-egyenletBal.getX()*i)){//Ha megegyezik a konstanssal
-					elozo.addVektor(new Vektor());//Ezt nem v·gom mÈg
-					g.drawImage(img, elozo.getX(), elozo.getY(), null);
-					try {Thread.sleep(210);} catch (InterruptedException e) {}
-				}
-				i++;
+							   kisRobot.getMezo().getPoziciovektor().getY());//Megv√°ltozott pozici√≥
+		if(uj.getX()!=elozo.getX() || uj.getY()!=elozo.getY()){//ha v√°ltoz√°s van, akkor kirajzoljuk a mozg√°st
+			Vektor kulonbseg = uj.addVektor2(elozo);
+			//kulonbseg.skalarOszt(2);
+			while(elozo.getX() == uj.getX() && elozo.getY() == uj.getY()) {
+				elozo.addVektor(kulonbseg);
+				g.drawImage(img, kulonbseg.getX(), kulonbseg.getY(), null);
 			}
-			
 			elozo.setVektor(uj);
-		}//Ha nincs, akkor kihagyjuk a fenti rÈszt
-		g.drawImage(img, elozo.getX(), elozo.getY(), null);//Rajzoljuk a helyÈre
+		}//Ha nincs, akkor kihagyjuk a fenti r√©szt
+		g.drawImage(img, elozo.getX(), elozo.getY(), null);//Rajzoljuk a hely√©re
 	}
 
 	/**
